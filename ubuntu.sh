01ef865a3420d13dc727fbaaea7f02c1620dba4c
simone <slysimo@gmail.com>
2017-12-14 04:59:49 +0100

    ops

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,9 @@
 
 # Install essential tools
 sudo apt update 
-sudo apt -y install git wget build-essential python python3
+sudo apt -y install git curl wget build-essential python python-pip python3 zsh
+
+pip3 install thefuck
 
 # Install multimedia tools/plugin and utilities
 sudo apt -y install va-driver-all libva1 gstreamer1.0-libav gstreamer1.0-vaapi
@@ -27,6 +29,7 @@ cd ..
 rm -dr nerd-fonts
 
 # Install oh-my-zsh
+chsh -s $(which zsh)
 echo "##### TYPE 'exit' IN ZSH SHELL TO CONTINUE INSTALL ######"
 read -p "Press any key..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

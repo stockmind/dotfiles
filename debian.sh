177048a499f1445f75ca0ef1c9c02dda0d659b38
stockmind <slysimo@gmail.com>
2017-12-14 04:41:34 +0100

    change default shell

--- debian.sh
+++ debian.sh
@@ -27,6 +27,7 @@ cd ..
 rm -dr nerd-fonts
 
 # Install oh-my-zsh
+chsh -s $(which zsh)
 echo "##### TYPE 'exit' IN ZSH SHELL TO CONTINUE INSTALL ######"
 read -p "Press any key..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"

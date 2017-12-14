7bcc76365789b68258ea4d5e122b810c04f9b504
simonerobertonunzi <slysimo@gmail.com>
2017-12-14 03:53:40 +0100

    sync locale

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dotfiles
+
+# Install on macOS
+
+Run and follow instructions
+
+	macos.sh
+
+# Install on Debian or derivates
+
+Run and follow instructions
+
+	debian.sh
+
+# Terminal themes
+
+Tomorrow Night theme for iTerm2 and Terminal
+[Repository](https://github.com/chriskempson/tomorrow-theme/)
---
+++ macos.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Prompt for developers tools install (git, etc...)
+xcode-select --install
+
+# Install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install brew packages
+brew update
+brew install $(< brewfile)
+brew tap caskroom/cask
+brew cask install $(< brewcaskfile)
+
+# Download scotch-box-pro vagrant file
+git clone https://github.com/scotch-io/scotch-box-pro.git
+
+# Install nerd fonts
+git clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh Hack
+cd ..
+rm -dr nerd-fonts
+
+# Install oh-my-zsh
+echo "##### TYPE 'exit' IN ZSH SHELL TO CONTINUE INSTALL ######"
+read -p "Press any key..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Copy .zshrc
+cp .zshrc ~/.zshrc

0262fc27aefdffae0632922071a246bf220b17d4
Simone Roberto Nunzi <slysimo@gmail.com>
2021-11-22 23:29:22 +0100

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -4,7 +4,7 @@
 xcode-select --install
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install brew packages
 brew update
@@ -13,32 +13,24 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     brew install $line
 done < brewfile
 
-brew tap caskroom/cask
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Install cask: $line"
-    brew cask install $line
+    brew install --cask $line
 done < brewcaskfile
 
-# MacOs color picker launcher
-brew tap nrlquaker/personal
-brew cask install colorpicker-launcher
-
-# Complete install of python3 for pip
-brew postinstall python3
-
-pip install Pygments
+pip3 install Pygments
 
 # Install nerd fonts
 brew tap homebrew/cask-fonts
-brew cask install font-hack-nerd-font
+brew install --cask font-hack-nerd-font
 
 # Install oh-my-zsh
 echo "##### TYPE 'exit' IN ZSH SHELL TO CONTINUE INSTALL ######"
 read -p "Press any key..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Copy .zshrc
 cp .zshrc ~/.zshrc
@@ -50,10 +42,10 @@ cp .zshrc ~/.zshrc
 #cp .vimrc ~/.vimrc
 
 # Uncomment to install OneDrive client from AppStore
-mas install 823766827
+# mas install 823766827
 # Install WhatsApp Desktop from App Store
 mas install 1147396723
 # Amphetamine
 mas install 937984704
 # Magnet
-mas install 441258766
+# mas install 441258766
